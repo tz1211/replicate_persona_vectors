52f8910388cd9941d171604a062dc3ca603db275
tz1211 <terryzhou1211@gmail.com>
2025-10-29 02:59:43 +0000

    update cal_projection.sh with qwen3-1.7B

--- scripts/cal_projection.sh
+++ scripts/cal_projection.sh
@@ -1,9 +1,10 @@
 gpu=${1:-0}
 
 
-CUDA_VISIBLE_DEVICES=$gpu python -m eval.cal_projection \
-    --file_path eval_persona_eval/Qwen2.5-7B-Instruct/evil.csv \
-    --vector_path persona_vectors/Qwen2.5-7B-Instruct/evil_response_avg_diff.pt \
+CUDA_VISIBLE_DEVICES=$gpu 
+uv run python -m eval.cal_projection \
+    --file_path output/eval_persona_eval/Qwen3-1.7B/evil.csv \
+    --vector_path output/persona_vectors/Qwen3-1.7B/evil_response_avg_diff.pt \
     --layer 20 \
-    --model_name Qwen/Qwen2.5-7B-Instruct \
+    --model_name Qwen/Qwen3-1.7B \
     --projection_type proj
